3bd9b121f9ddf0689ddda6d329f9296a2547ae12
khjang <comforme0@naver.com>
2020-05-08 16:33:43 +0900

    photo upload

---
+++ app/src/main/java/com/example/mystar/navigation/AddPhotoActivity.kt
@@ -0,0 +1,108 @@
+package com.example.mystar.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mystar.R
+import com.example.mystar.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+
+    private var PIC_IMAGE_FROM_ALBUM = 0
+    var storage: FirebaseStorage? = null
+    var photoUri: Uri? = null
+
+    var auth:  FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PIC_IMAGE_FROM_ALBUM)
+
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PIC_IMAGE_FROM_ALBUM) {
+            if (resultCode == Activity.RESULT_OK) {
+                photoUri = data?.data
+                addphoto_image.setImageURI(photoUri)
+
+            } else {
+                finish()
+            }
+        }
+    }
+
+    private fun contentUpload() {
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_"+timestamp+"_.png"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        /*storageRef?.putFile(photoUri!!)?.continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            contentDTO.imageUrl = uri.toString()
+
+            contentDTO.uid = auth?.currentUser?.uid
+
+            contentDTO.userId = auth?.currentUser?.email
+
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
+        }*/
+//
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var contentDTO = ContentDTO()
+
+                contentDTO.imageUrl = uri.toString()
+
+                contentDTO.uid = auth?.currentUser?.uid
+
+                contentDTO.userId = auth?.currentUser?.email
+
+                contentDTO.explain = addphoto_edit_explain.text.toString()
+
+                contentDTO.timestamp = System.currentTimeMillis()
+
+                firestore?.collection("images")?.document()?.set(contentDTO)
+
+                setResult(Activity.RESULT_OK)
+
+                finish()
+            }
+        }
+
+    }
+}
