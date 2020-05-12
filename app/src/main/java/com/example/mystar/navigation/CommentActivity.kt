835c5f683bd93279ef8c7740a5fa54b1aec81280
khjang <comforme0@naver.com>
2020-05-12 16:42:12 +0900

    comment

---
+++ app/src/main/java/com/example/mystar/navigation/CommentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mystar.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
